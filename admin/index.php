df50ea56641adb4803b5768bafea354ce75c03b5
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-26 12:33:10 +0600

    some done

---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Food order website home page</title>
+  <link rel="stylesheet" href="/css/admin.css">
+</head>
+<body>
+   <!--Menu section starts-->
+   <div class='menu'>
+     Menu goes here
+   </div>
+   <!--Menu section ends-->
+
+   <!--Main content  section starts-->
+   <div class='main-content'>
+     Main content goes here
+   </div>
+   <!--Main content section ends-->
+
+
+   <!--Footer section starts-->
+   <div class='footer'>
+     footer goes here
+   </div>
+   <!--Footer section ends-->
+</body>
+</html>
